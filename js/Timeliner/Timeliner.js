21e5332f397a2c93d784f9e74dd59a76a72885d7
sius012 <dezoditing012@gmail.com>
2023-11-22 16:01:58 +0700

    check date function

--- js/Timeliner/Timeliner.js
+++ js/Timeliner/Timeliner.js
@@ -1,3 +1,28 @@
-$.fn.Timeliner = function () {
-    console.log(this.length);
+function getDaysInMonth(year) {
+    const daysInMonth = [];
+
+    let namaBulan = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember"
+      ];
+    for (let month = 0; month < 12; month++) {
+      daysInMonth.push({name: namaBulan[month],count: new Date(year, month + 1, 0).getDate()});
+    }
+    return daysInMonth;
+}
+
+
+$.fn.Timeliner = function (params = {title: "title", start_date: "start_date", end_date: "end_date"}) {
+    console.log(getDaysInMonth(2023))
+      
 };
